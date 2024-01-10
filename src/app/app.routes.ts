d0a9e52cdb6116283a4fbb274776b6a5c0c01c08
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2024-01-11 01:05:47 +0700

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {OrderComponent} from "./components/order/order.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  // { path: 'products/:id', component: DetailProductComponent },
+  // { path: 'orders', component: OrderComponent,canActivate:[AuthGuardFn] },
+  // { path: 'user-profile', component: UserProfileComponent, canActivate:[AuthGuardFn] },
+  // { path: 'orders/:id', component: OrderDetailComponent },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { HomeComponent } from './app/components/home/home.component';
-import {OrderComponent} from "./app/components/order/order.component";
-import {ProductDetailComponent} from "./app/components/product-detail/product-detail.component";
-import {OrderConfirmComponent} from "./app/components/order-confirm/order-confirm.component";
+import {AppComponent} from "./app/app/app.component";
 
-bootstrapApplication(OrderConfirmComponent, appConfig)
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
