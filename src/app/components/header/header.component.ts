1694a89cba11afaac1f67e090f2105781a5af93f
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2024-01-17 02:06:56 +0700

    update header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserResponse } from '../../responses/user/user.response';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import {StorageService} from "../../services/storage.service";
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    NgbModule,
+    RouterModule
+  ]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  userResponse?:UserResponse | null;
+  isPopoverOpen = false;
+  activeNavItem: number = 0;
 
+  constructor(
+    private userService: UserService,
+    private storageService: StorageService,
+    private router: Router,
+  ) {
+
+  }
+  ngOnInit() {
+    this.userResponse = this.userService.getUserResponseFromLocalStorage();
+  }
+
+  togglePopover(event: Event): void {
+    event.preventDefault();
+    this.isPopoverOpen = !this.isPopoverOpen;
+  }
+
+  handleItemClick(index: number): void {
+    //alert(`Clicked on "${index}"`);
+    if(index === 0) {
+      debugger
+      this.router.navigate(['/user-profile']);
+    } else if (index === 2) {
+      this.userService.removeUserFromLocalStorage();
+      this.storageService.removeToken();
+      this.userResponse = this.userService.getUserResponseFromLocalStorage();
+    }
+    this.isPopoverOpen = false; // Close the popover after clicking an item
+  }
+
+
+  setActiveNavItem(index: number) {
+    this.activeNavItem = index;
+    //alert(this.activeNavItem);
+  }
 }
