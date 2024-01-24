acafc9bd431ce4db69b20ed6af1981009d66dc7c
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2024-01-25 01:23:25 +0700

    remove debugger

--- src/app/components/admin/product/product-add/product-add.component.ts
+++ src/app/components/admin/product/product-add/product-add.component.ts
@@ -40,11 +40,7 @@ export class ProductAddComponent {
   getCategories(page: number, limit: number) {
     this.categoryService.getCategories(page, limit).subscribe({
       next: (categories: Category[]) => {
-        debugger
         this.categories = categories;
-      },
-      complete: () => {
-        debugger;
       },
       error: (error: any) => {
         console.error('Error fetching categories:', error);
@@ -67,29 +63,20 @@ export class ProductAddComponent {
   insertProduct() {
     this.productService.insertProduct(this.insertProductDTO).subscribe({
       next: (response) => {
-        debugger
         if (this.insertProductDTO.images.length > 0) {
           const productId = response.id; // Assuming the response contains the newly created product's ID
           this.productService.uploadImages(productId, this.insertProductDTO.images).subscribe({
             next: (imageResponse) => {
-              debugger
-              // Handle the uploaded images response if needed
               console.log('Images uploaded successfully:', imageResponse);
-              // Navigate back to the previous page
               this.router.navigate(['../'], {relativeTo: this.route});
             },
             error: (error) => {
-              // Handle the error while uploading images
-              alert(error.error)
               console.error('Error uploading images:', error);
             }
           })
         }
       },
       error: (error) => {
-        debugger
-        // Handle error while inserting the product
-        alert(error.error)
         console.error('Error inserting product:', error);
       }
     });
